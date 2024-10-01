6e4025221ae896647c64b484638ac50d4e5f2deb
Nokiloki700 <lenard700@gmail.com>
2024-10-01 09:05:27 +0200

    cLm

--- f2/f2.js
+++ f2/f2.js
@@ -1,8 +1,10 @@
 const students = require('../students.json');
 /* Írj függvényt ami feltölt egy objektumot (lehetőleg üres {}-ről induljon) 
 a klubbok nevével és tagjainak számával. (pl. {"club1": 4, "club2": 3....}) */
-function f2() {
-    //TODO
-}
-
+const clubs = {};
+students.forEach(student => {
+    let clubName = student.club;
+    if (!clubs[clubName]) clubs[clubName] = 1;
+    else clubs[clubName]++;
+});
 module.exports = f2;
